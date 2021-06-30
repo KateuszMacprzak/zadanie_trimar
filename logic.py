5ee802a42fea17e5ea0c21e091ad62453ca6f686
Mateusz Kacprzak <matkac98@gmail.com>
2021-06-30 11:07:27 +0200

    Stworzenie logiki programu

--- logic.py
+++ logic.py
@@ -1,1 +1,17 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/<number>')
+def give_a_number(number):
+    try:
+        if int(number)%2==0:
+            return 'This number is divide by 2'
+        if int(number)%2!=0:
+            return 'This number is not divide by 2'
+    except ValueError:
+        return 'That is wrong type. You must enter a int type !'
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
